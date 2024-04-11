df4fe5e0faf6e19cbb8eb85197c36a4a047abc91
giang.dongoc <giang.dongoc@vti.com.vn>
2024-04-11 14:16:37 +0700

    🔥 Go variables

--- main.go
+++ main.go
@@ -4,11 +4,28 @@ import (
 	"fmt"
 )
 
+// Variable types
+/*
+	- int: 32-bit signed integer
+	- float32: 32-bit floating point
+	- string: text
+	- bool: true or false
+*/
+
+// Declaring
+/*
+	- var <name> <type> = <value>
+		var student1 string = "John" //type is string
+  	var student2 = "Jane" //type is inferred
+  	X := 2 //type is inferred
+*/
+
 func main() {
-	// comment one line
-	/*
-		comment multi line
-		comment multi line
-	*/
-	fmt.Println("HaUI Food Payment Service")
+	var fullname = "Do Ngoc Giang"
+	var age int
+	age = 21
+	address := "Hanoi"
+	fmt.Println("Hello, ", fullname)
+	fmt.Println(age)
+	fmt.Println(address)
 }
