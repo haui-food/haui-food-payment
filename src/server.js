9f199312ac4804393cce208c155bd5275ed65ec0
dngiang <dongocgiang.edu@gmail.com>
2024-05-12 20:52:04 +0700

    ✨ Connect databse mongo

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const env = require('./config');
 
@@ -8,6 +9,13 @@ app.all('*', (req, res) => {
   res.send('Service payment is running 🌱');
 });
 
-app.listen(env.port, () => {
-  console.log(`Server running on port ${env.port}`);
-});
+mongoose
+  .connect(env.mongoURI)
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .then(() => {
+    app.listen(env.port, () => {
+      console.log(`Server running on port ${env.port}`);
+    });
+  });
