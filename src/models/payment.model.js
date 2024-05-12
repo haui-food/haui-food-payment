d0947a6b1c8465177486a44150a8e9c6a608f532
dngiang <dongocgiang.edu@gmail.com>
2024-05-12 22:34:12 +0700

    ✨ Update model payment

--- src/models/payment.model.js
+++ src/models/payment.model.js
@@ -1,19 +1,20 @@
 const mongoose = require('mongoose');
 
-const paymentSchema = mongoose.Schema({
-  description: {
-    type: String,
+const paymentSchema = mongoose.Schema(
+  {
+    description: {
+      type: String,
+    },
+    amount: {
+      type: Number,
+    },
+    runningBalance: {
+      type: Number,
+    },
   },
-  amount: {
-    type: Number,
+  {
+    timestamps: true,
   },
-  runningBalance: {
-    type: Number,
-  },
-  dateTime: {
-    type: Date,
-    default: Date.now,
-  },
-});
+);
 
 module.exports = mongoose.model('Payment', paymentSchema);
