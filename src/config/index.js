81e9c10ffd89a574bb3433a25af7045a50af9b0f
giang.dongoc <giang.dongoc@vti.com.vn>
2024-05-13 11:21:58 +0700

    ✨ Add account number

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,7 @@ const env = {
   port: +process.env.PORT || 3000,
   mongoURI: process.env.MONGO_URI,
   keyHash: process.env.KEY_HASH,
+  accountNo: process.env.ACCOUNT_NO,
   usernameHash: process.env.USERNAME_HASH,
   passwordHash: process.env.PASSWORD_HASH,
 };
