25e970aad2ab55623117f973c365d39f1f29cff6
dngiang <dongocgiang.edu@gmail.com>
2024-05-27 00:14:59 +0700

    ✨ Add base send message payment

--- src/services/tp-bank.service.js
+++ src/services/tp-bank.service.js
@@ -1,18 +1,5 @@
 const axios = require('axios');
 
-const env = require('../config');
-const apiService = require('./api.service');
-const userService = require('./user.service');
-const cacheService = require('./cache.service');
-const cryptoService = require('./crypto.service');
-const paymentService = require('./payment.service');
-const { usernameHash, passwordHash, accountNo } = require('../config');
-const {
-  KEY_ACCESS_TOKEN,
-  KEY_LIST_PAYMENTS,
-  DATE_NUMBER_DIFFERENCE,
-  TIME_CACHE_LIST_PAYMENTS,
-} = require('../constants');
 const {
   getDeviceId,
   getRandomNumber,
@@ -20,6 +7,19 @@ const {
   extractDynamicValue,
   findDifferentElements,
 } = require('../utils');
+const {
+  KEY_ACCESS_TOKEN,
+  KEY_LIST_PAYMENTS,
+  DATE_NUMBER_DIFFERENCE,
+  TIME_CACHE_LIST_PAYMENTS,
+} = require('../constants');
+const env = require('../config');
+const apiService = require('./api.service');
+const userService = require('./user.service');
+const cacheService = require('./cache.service');
+const cryptoService = require('./crypto.service');
+const paymentService = require('./payment.service');
+const { usernameHash, passwordHash, accountNo } = require('../config');
 
 const username = cryptoService.decrypt(usernameHash);
 const password = cryptoService.decrypt(passwordHash);
@@ -101,14 +101,14 @@ const getTransactionHistory = async () => {
   }
 
   const requestData = {
-    pageNumber: 1,
-    pageSize: 400,
-    accountNo,
-    currency: 'VND',
-    maxAcentrysrno: '',
     toDate,
     fromDate,
+    accountNo,
     keyword: '',
+    pageSize: 400,
+    pageNumber: 1,
+    currency: 'VND',
+    maxAcentrysrno: '',
   };
 
   const config = {
@@ -124,7 +124,6 @@ const getTransactionHistory = async () => {
   };
 
   try {
-    console.log('Get money');
     const response = await axios(config);
     const { status, data: responseData } = response;
 
@@ -136,7 +135,6 @@ const getTransactionHistory = async () => {
       let paymentsExistIds = cacheService.get(KEY_LIST_PAYMENTS) || [];
 
       if (!paymentsExistIds.length) {
-        console.log('Get payments exist database');
         paymentsExistIds = await paymentService.getListTransactionIds();
         cacheService.set(KEY_LIST_PAYMENTS, paymentsExistIds, TIME_CACHE_LIST_PAYMENTS);
       }
@@ -147,10 +145,8 @@ const getTransactionHistory = async () => {
         newIdsTransaction.includes(transactionInfo.id),
       );
 
-      console.log('Total balance:', transactionInfos[0].runningBalance);
-
-      if (newTransactionRaw.length) {
-        for (const newTransaction of newTransactionRaw) {
+      if (newTransactionRaw.length > 0) {
+        for (let newTransaction of newTransactionRaw) {
           const { id: transactionId, amount, description, runningBalance } = newTransaction;
 
           const newPayment = {
@@ -162,46 +158,31 @@ const getTransactionHistory = async () => {
 
           await paymentService.createNew(newPayment);
 
-          let dataSend = { desc, status: 'SUCCESS', amount, balance: runningBalance };
           const desc = extractDynamicValue(description);
 
-          const isRecharge = checkPaymentType(description);
+          let dataSend = { desc, amount };
 
-          if (isRecharge) {
-            if (amount >= env.minAmount) {
-              userService.updateBalanceByUsername(desc, +amount);
-            }
+          const isRecharge = desc.includes('hauifood');
+
+          if (isRecharge && amount > env.minAmount) {
+            await userService.updateBalanceByUsername(desc, +amount);
           }
 
           dataSend = { ...dataSend, method: isRecharge ? 'recharge' : 'payment' };
 
-          await apiService.sendMessage(dataSend);
+          apiService.sendMessage(dataSend);
+
+          cacheService.set(
+            KEY_LIST_PAYMENTS,
+            new Set([...paymentsExistIds, ...newIdsTransaction]),
+            TIME_CACHE_LIST_PAYMENTS,
+          );
         }
-        console.log('Old transaction: ', paymentsExistIds);
-
-        cacheService.set(
-          KEY_LIST_PAYMENTS,
-          new Set([...paymentsExistIds, ...newIdsTransaction]),
-          TIME_CACHE_LIST_PAYMENTS,
-        );
-        const newTransaction = cacheService.get(KEY_LIST_PAYMENTS) || [];
-
-        console.log('New transaction: ', newTransaction);
       }
-
-      return newTransactionRaw;
     }
-
-    return null;
   } catch (error) {
     console.log(error.response.data);
-    return null;
   }
-};
-
-const checkPaymentType = (description) => {
-  const isRecharge = description.includes('hauifood');
-  return isRecharge;
 };
 
 module.exports = {
